0ac8cfddd54631a821032caaddde7cb9cf537ddb
shinfu <shinfu619@gmail.com>
2020-09-25 11:46:40 +0800

    v1.0

---
+++ cornerDetection.cpp
@@ -0,0 +1,131 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace cv;
+using namespace std;
+
+Mat goodFeaturesTrack(Mat, Mat, int, RNG);
+Mat cornerHarrisTrack(Mat, Mat, int, RNG);
+
+int main(int argc, char* argv[])
+{
+    int numCorners = 30;
+
+    RNG rng(12345);
+    string windowName = "Feature points";
+
+    Mat frame, frameGray;
+
+    char ch;
+    float scalingFactor = 0.25;
+
+    const string& fileName = "river.MOV";
+    VideoCapture inputVideo(fileName);
+
+    if (!inputVideo.isOpened()) {
+        cerr << "Failed to open the video." << endl;
+        return EXIT_FAILURE;
+    }
+
+    // Video output
+    const string video_name = "output.mp4";
+    int video_FourCC = static_cast<int>(inputVideo.get(CAP_PROP_FOURCC));     // Get Codec Type
+    int video_fps = inputVideo.get(CAP_PROP_FPS);
+    Size video_size = Size(int(inputVideo.get(CAP_PROP_FRAME_WIDTH)*0.5),
+                           int(inputVideo.get(CAP_PROP_FRAME_HEIGHT))*0.25);
+
+    VideoWriter outputVideo;
+    outputVideo.open(video_name, video_FourCC, video_fps, video_size, true);
+
+    
+
+    while (true) {
+        if (!inputVideo.read(frame)) break;
+        
+        resize(frame, frame, Size(), scalingFactor, scalingFactor, INTER_AREA);
+        cvtColor(frame, frameGray, COLOR_BGR2GRAY);
+
+        Mat frameGoodFeatures = goodFeaturesTrack(frame, frameGray, numCorners, rng);
+        Mat frameHarris = cornerHarrisTrack(frame, frameGray, numCorners, rng);
+
+        // stack two frames
+        Mat frameShow;
+        hconcat(frameGoodFeatures, frameHarris, frameShow);
+
+        outputVideo.write(frameShow);
+        imshow(windowName, frameShow);
+
+        ch = waitKey(1);
+        if (ch == 27) {
+            break;
+        }
+    }
+
+    inputVideo.release();
+    destroyAllWindows();
+
+    return EXIT_SUCCESS;
+}
+
+
+Mat goodFeaturesTrack(Mat frame, Mat frameGray, int numCorners, RNG rng) {
+
+    // Init the params for Shi-Tomasi algorithm
+    vector<Point2f> corners;
+    double qualityThreshold = 0.02;
+    double minDist = 15;
+    int blockSize = 5;
+    bool useHarrisDetector = false;
+    double k = 0.05;
+
+    Mat frameCopy = frame.clone();
+
+    // corner detection
+    goodFeaturesToTrack(frameGray, corners, numCorners, qualityThreshold, minDist,
+                        Mat(), blockSize, useHarrisDetector, k);
+
+    // Draw the detected corners by circles
+    for (size_t i = 0; i < corners.size(); i++) {
+        Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+        circle(frameCopy, corners[i], 8, color, 2, 2, 0);
+    }
+
+    return frameCopy;
+}
+
+
+Mat cornerHarrisTrack(Mat frame, Mat frameGray, int numCorners, RNG rng) {
+
+    // Init params
+    Mat dst, dst_norm, dst_norm_scaled;
+    int apertureSize = 5;
+    double k = 0.04;
+    int blockSize = 2;
+    int thresh = 200;    
+    dst = Mat::zeros(frame.size(), CV_32FC1);
+
+    // Detecting corners
+    cornerHarris(frameGray, dst, blockSize, apertureSize, k, BORDER_DEFAULT);
+
+    normalize(dst, dst_norm, 0, 255, NORM_MINMAX, CV_32FC1, Mat());
+    convertScaleAbs(dst_norm, dst_norm_scaled);
+
+    // Draw the detected corners by circles
+    for (int j = 0; j < dst_norm.rows; j++)
+    {
+        for (int i = 0; i < dst_norm.cols; i++)
+        {
+            if ((int)dst_norm.at<float>(j, i) > thresh)
+            {
+                Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+                circle(frame, Point(i, j), 8, color, 2, 8, 0);
+            }
+        }
+    }
+
+    return frame;
+}
---
+++ lucasKandeTracker.cpp
@@ -0,0 +1,149 @@
+#include "opencv2/video/tracking.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+bool pointTrackingFlag = false;
+Point2f currentPoint;
+
+void onMouse(int event, int x, int y, int, void*);
+
+int main(int argc, char* argv[])
+{
+    string file_name = "landing.mov";
+    VideoCapture inputVideo(file_name);
+
+    if (!inputVideo.isOpened())
+    {
+        cerr << "Unable to open the webcam or video." << endl;
+        return EXIT_FAILURE;
+    }
+
+    TermCriteria terminationCriteria(TermCriteria::COUNT | TermCriteria::EPS, 10, 0.02);
+
+    Size windowSize(25, 25);
+
+    // Maximum number of points want to track
+    const int maxNumPoints = 200;
+
+    string windowName = "Lucas Kanade Tracker";
+    namedWindow(windowName);
+    setMouseCallback(windowName, onMouse, 0);
+
+    Mat prevGrayImage, curGrayImage, image, frame;
+    vector<Point2f> trackingPoints[2];
+
+    // Video output
+    const string video_name = "output.mp4";
+    int video_FourCC = static_cast<int>(inputVideo.get(CAP_PROP_FOURCC));     // Get Codec Type
+    // int video_FourCC = VideoWriter::fourcc('M', 'J', 'P', 'G');
+    int video_fps = inputVideo.get(CAP_PROP_FPS);
+    Size video_size = Size(int(inputVideo.get(CAP_PROP_FRAME_WIDTH)),
+                           int(inputVideo.get(CAP_PROP_FRAME_HEIGHT)));
+
+    VideoWriter outputVideo;
+    outputVideo.open(video_name, video_FourCC, video_fps, video_size, true);
+
+    while (true)
+    {   
+        if (!inputVideo.read(frame)) break;
+        
+        frame.copyTo(image);
+        //resize(frame, image, Size(0, 0), 0.5, 0.5);          
+        cvtColor(image, curGrayImage, COLOR_BGR2GRAY);
+
+        // Check if there are points to track
+        if (!trackingPoints[0].empty())
+        {
+            vector<uchar> statusVector;
+            vector<float> errorVector;
+
+            if (prevGrayImage.empty())
+            {
+                curGrayImage.copyTo(prevGrayImage);
+            }
+
+            // Calculate the optical flow using Lucas-Kanade algorithm
+            calcOpticalFlowPyrLK(prevGrayImage, curGrayImage, 
+                                 trackingPoints[0], trackingPoints[1], 
+                                 statusVector, errorVector, windowSize, 3, 
+                                 terminationCriteria, 0, 0.001);
+
+            int count = 0;           
+            int minDist = 7;
+
+            for (int i = 0; i < trackingPoints[1].size(); i++)
+            {
+                if (pointTrackingFlag)
+                {
+                    // If the new point is within 'minDist' distance from an existing point, it will not be tracked
+                    if (norm(currentPoint - trackingPoints[1][i]) <= minDist)
+                    {
+                        pointTrackingFlag = false;
+                        continue;
+                    }
+                }
+
+                // Check the status vector
+                if (!statusVector[i])
+                    continue;
+
+                trackingPoints[1][count++] = trackingPoints[1][i];
+
+                int radius = 16;
+                int thickness = 2;
+                int lineType = 8;
+                circle(image, trackingPoints[1][i], radius, Scalar(0, 0, 255), thickness, lineType);
+            }
+
+            trackingPoints[1].resize(count);
+        }
+
+        // Refining the location of the feature points
+        if (pointTrackingFlag && trackingPoints[1].size() < maxNumPoints)
+        {
+            vector<Point2f> tempPoints;
+            tempPoints.push_back(currentPoint);
+
+            // refine the location of the corners to subpixel accuracy.
+            cornerSubPix(curGrayImage, tempPoints, windowSize, Size(-1, -1), terminationCriteria);
+
+            trackingPoints[1].push_back(tempPoints[0]);
+            pointTrackingFlag = false;
+        }
+
+
+        outputVideo.write(image);
+
+        imshow(windowName, image);
+        
+        char ch = waitKey(100);
+        if (ch == 27)
+            break;
+
+        // Swap the 'points' vectors to update 'previous' to 'current'
+        std::swap(trackingPoints[1], trackingPoints[0]);
+
+        // Swap the images to update previous image to current image
+        cv::swap(prevGrayImage, curGrayImage);
+    }
+
+    inputVideo.release();
+    outputVideo.release();
+
+    return EXIT_SUCCESS;
+}
+
+
+void onMouse(int event, int x, int y, int, void*)
+{
+    if (event == EVENT_LBUTTONDOWN)
+    {
+        currentPoint = Point2f((float)x, (float)y);
+        pointTrackingFlag = true;
+    }    
+}
